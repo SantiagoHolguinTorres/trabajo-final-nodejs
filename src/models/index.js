3b2b2333800f572e52d03377f6675e472ddfbbe7
SantiagoHolguinTorres <157505483+SantiagoHolguinTorres@users.noreply.github.com>
2024-06-27 22:56:40 -0500

    advances

--- src/models/index.js
+++ src/models/index.js
@@ -1,2 +1,18 @@
+const Cart = require('./Cart')
+const Category = require('./Category')
+const Product = require('./Prduct')
+const User = require('./User')
+
 require('./User')
 require('./Category')
+require('./Prduct')
+require('./Cart')
+
+Product.belongsTo(Category) //-> Product -> categoryId 
+Category.hasMany(Product)
+
+Cart.belongsTo(User)
+User.hasMany(Cart)
+
+Cart.belongsTo(Product)
+Product.hasMany(Cart)
